d87cca4193db435f20495930b9acf34acf344fb9
Gabriel <gabriel.estudo1033@gmail.com>
2025-09-08 00:02:41 -0300

    atualização do projeto

--- geradordesenhas.py
+++ geradordesenhas.py
@@ -1,15 +1,28 @@
 import string
 import random
+import secrets
 
-
-def gerar_senha(comprimento):
+def gerar_senha(tamanho=12):
+    # Definindo o conjunto de caracteres para a senha
     caracteres = string.ascii_letters + string.digits + string.punctuation
-    senha = []
+    # Gerando a senha de forma segura
+    senha = ''.join(secrets.choice(caracteres) for _ in range(tamanho))
+    return senha
 
-    for i in range(comprimento):
-        caractere = random.choice(caracteres)
-        senha.append(caractere)
-    return ''.join(senha)
+def main():
+    print("Gerador de Senha Aleatória e Segura")
+    tamanho = int(input("Digite o tamanho da senha desejada: "))
+    
+    senha_gerada = gerar_senha(tamanho)
+    print(f"Senha gerada: {senha_gerada}")
 
-senha = gerar_senha(12)
-print(senha)
+    # Copiar a senha para a área de transferência
+    try:
+        import pyperclip
+        pyperclip.copy(senha_gerada)
+        print("Senha copiada para a área de transferência!")
+    except ImportError:
+        print("Para copiar a senha, instale o módulo pyperclip com 'pip install pyperclip'.")
+
+if __name__ == "__main__":
+    main()
